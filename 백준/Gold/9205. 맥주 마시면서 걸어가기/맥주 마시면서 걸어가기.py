b23adb58cdfd7fcf770b05841e73a569832f7421
juyong31 <85395058+juyong31@users.noreply.github.com>
2023-09-13 21:04:46 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 160 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,32 @@
+from collections import deque
+res = []
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  startX, startY = map(int, input().split())
+  store = []
+  for _ in range(n):
+    store.append(list(map(int, input().split())))
+  endX, endY = map(int, input().split())
+  visited = [False for _ in range(n+1)]
+  
+  def bfs():
+    queue = deque()
+    queue.append([startX, startY])
+    while queue:
+      x, y = queue.popleft()
+      if(abs(endX-x)+abs(endY-y) <= 1000):
+        res.append('happy')
+        return
+      for i in range(n):
+        ix, iy = store[i][0], store[i][1]
+        if(abs(ix-x) + abs(iy-y) <= 1000 and visited[i] == False):
+          queue.append([ix,iy])
+          visited[i] = True
+    
+    res.append('sad')
+
+  bfs()
+for v in res:
+  print(v)
