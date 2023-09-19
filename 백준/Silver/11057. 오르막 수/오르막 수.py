ef99e89bb5f70b472cdba91dce4a46fa1390459f
juyong31 <85395058+juyong31@users.noreply.github.com>
2023-09-19 14:54:05 +0900

    [Silver I] Title: 오르막 수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+dp = [[1]*10 for _ in range(n+1)]
+
+if(n >= 2):
+  for i in range(2, n+1):
+    for j in range(1,10):
+      dp[i][j] = dp[i-1][j] + dp[i][j-1]
+      
+print(sum(dp[n])%10007)
